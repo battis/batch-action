0a102d6aa7fe7166c0eff43e8b4f775095fe3e88
Seth Battis <sethbattis@stmarksschool.org>
2016-03-16 11:29:01 -0400

    Noodling

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require_once ('vendor/autoload.php');
+
+use Battis\BatchAction\BatchManager;
+use Battis\BatchAction\Batch;
+use Battis\BatchAction\Filter;
+
+date_default_timezone_set('America/New_York');
+try {
+	$installer = BatchManager::getInstance();
+	$installer->run(true);
+	$installer->run(true, (new Filter())->add(Batch::SCRIPT()));
+	$installer->run(true, (new Filter())->add(Batch::SCRIPT(), 1));
+	$installer->run(true, 
+		(new Filter())->add(Batch::SCRIPT(), 1)->add(Batch::DATABASE()));
+} catch (Battis\BatchAction\Exception $e) {
+	echo '<p>' . $e->getMessage() . '</p>';
+}
+
+?>
+<pre><?= file_get_contents('BatchManager.log') ?></pre>
